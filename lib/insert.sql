98e15fcce8bf8a17da0e33be540b011ebf125ecb
https://learn.co/DenaNeuman <denaweiss5@gmail.com>
2020-11-05 10:24:05 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
+(1, null, 8, 'F', 'White', 'Calm', True);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
+(2, 'Mr. Chocolate', 23, 'M', 'Black', 'Loud', True);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
+(3, 'Rowdy', 24, 'M', 'Black', 'Sassy', True);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
+(4, 'Tabitha', 25, 'F', 'Brown', 'Loud', True);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
+(5, 'Sergeant Brown', 29, 'M', 'Mixed', 'Calm', True);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
+(6, 'Melissa', 2, 'F', 'Red', 'Sassy', True);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
+(7, 'Grinch', 30, 'M', 'Black', 'Loud', False);
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
+(8, 'Wendy', 8, 'F', 'White', 'Calm', True);
